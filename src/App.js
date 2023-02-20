af09073fde997dff1f45a1272591bd51a9e915cf
luis eduardo canedo torres <luillys83@yahoo.es>
2023-02-20 17:09:36 -0500

    Mejoras

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Mi currículum</h1>
+        <h1>Mi currículum</h1>
         <nav>
           <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Experiencia</a></li>
-            <li><a href="#">Educación</a></li>
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">Experiencia</a>
+            </li>
+            <li>
+              <a href="#">Educación</a>
+            </li>
           </ul>
         </nav>
       </header>
       <main>
         <section>
           <h2>Resumen profesional</h2>
-          <p>Soy un desarrollador web con experiencia en React, Angular y Node.js.</p>
+          <p>
+            Soy un desarrollador web con experiencia en React, Angular y
+            Node.js.
+          </p>
         </section>
         <section>
           <h2>Experiencia laboral</h2>
           <ul>
             <li>
-              <p><strong>Desarrollador web senior</strong></p>
+              <p>
+                <strong>Desarrollador web senior</strong>
+              </p>
               <p>ABC Company</p>
               <p>Octubre 2018 - presente</p>
             </li>
             <li>
-              <p><strong>Desarrollador web junior</strong></p>
+              <p>
+                <strong>Desarrollador web junior</strong>
+              </p>
               <p>XYZ Company</p>
               <p>Enero 2017 - Septiembre 2018</p>
             </li>
@@ -38,7 +50,9 @@ function App() {
           <h2>Educación</h2>
           <ul>
             <li>
-              <p><strong>Licenciatura en Informática</strong></p>
+              <p>
+                <strong>Licenciatura en Informática</strong>
+              </p>
               <p>Universidad Nacional Autónoma de México</p>
               <p>2012 - 2016</p>
             </li>
